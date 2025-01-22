dd590697f057cfaaf4c9230b11e26f48993e58d0
Vectorian-code <eloanyiemmanuel@gmail.com>
2025-01-22 21:18:26 +0100

    Update phish.py

--- phish.py
+++ phish.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
